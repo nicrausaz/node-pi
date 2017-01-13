dad45d8b1b6ad8201d50df91783828479e2884eb
nicrausaz <n.crausaz99@gmail.com>
2017-01-13 22:59:25 +0100

    add style template

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendfile(path.join(__dirname + '/public/index.html'))
 })
 
 app.listen(3000, function () {
