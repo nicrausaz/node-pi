52e4c630fe5f333aec40459d11415bad17464e5b
nicrausaz <n.crausaz99@gmail.com>
2017-01-15 20:41:15 +0100

    add Note front end done

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+var http = require('http');
 var express = require('express')
 var app = express()
 var path = require('path');
 
 app.use(express.static('public'));
+app.use(express.bodyParser());
 
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname + '/public/index.html'))
 })
+
+app.post('/newNote', function(req, res){
+    res.send(req.body.newNote.note);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
