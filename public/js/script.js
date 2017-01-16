d14751cf42a25aed01fe09892f530316f8df0e0e
nicrausaz <n.crausaz99@gmail.com>
2017-01-16 21:21:31 +0100

    switches and show/hide working

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,27 @@
 $(document).ready(function () {
     console.log("doc is ready");
-    checkAddNote();
+    showHideDivs();
+    switchButtons();
 });
 
-function checkAddNote() {
+function showHideDivs() {
     $('#btnAddNoteDiv').click(function () {
         $('#addNoteDiv').show(500);
-        $('#btnAddNoteDiv').removeClass("btn-primary").addClass("btn-default");
+        $('#showNoteDiv').hide(500);
     });
     $('#btnShowNotesDiv').click(function () {
         $('#addNoteDiv').hide(500);
+        $('#showNoteDiv').show(500);
+    });
+}
+
+function switchButtons() {
+    $('#btnAddNoteDiv').click(function () {
         $('#btnAddNoteDiv').removeClass("btn-default").addClass("btn-primary");
+        $('#btnShowNotesDiv').removeClass("btn-primary").addClass("btn-default");
+    });
+    $('#btnShowNotesDiv').click(function () {
+        $('#btnShowNotesDiv').removeClass("btn-default").addClass("btn-primary");
+        $('#btnAddNoteDiv').removeClass("btn-primary").addClass("btn-default");
     });
 }
