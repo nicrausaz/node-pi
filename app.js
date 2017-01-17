a696d6f450edb7263534f899c5a0a0055c81d2f0
nicrausaz <n.crausaz99@gmail.com>
2017-01-17 21:02:05 +0100

    get req.body working

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var http = require('http')
 var express = require('express')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
 
 var app = express()
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 //var User = require('schemas/User.js')
 
-mongoose.connect('mongodb://nodePiServiceAccount:1234@192.168.1.121:27017/node-pi')
+//mongoose.connect('mongodb://nodePiServiceAccount:1234@192.168.1.121:27017/node-pi')
 
 
 // var db = mongoose.connection
@@ -21,12 +27,14 @@ mongoose.connect('mongodb://nodePiServiceAccount:1234@192.168.1.121:27017/node-p
 
 app.use(express.static('public'))
 
-app.get('/login', function (req, res) {
 
-  var username = req.body.username
-  var password = req.body.password
-  console.log(username + password)
 
+app.post('/login', urlencodedParser, function (req, res) {
+
+  // var username = req.body.username
+  // var password = req.body.password
+  console.log(req.body)
+  res.redirect('index.html')
 });
 
 app.get('/newNote', function (req, res) {
